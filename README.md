507ade8cb4858c02b3701648b93e873cf4b40c43
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-11-11 12:03:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+npm i redux-persist
+
+https://blog.logrocket.com/persist-state-redux-persist-redux-toolkit-react/
+
+FOR REACT NATIVE:
+https://blog.logrocket.com/use-redux-persist-react-native/#:~:text=In%20React%20Native%20applications%2C%20data,Redux%20state%20object%20to%20AsyncStorage%20.
+
+const persistConfig = {
+key: 'root',
+storage: AsyncStorage,
+}
---
+++ src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
+
+/* Before:
+
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
+
+export const store = configureStore({
+  reducer: userReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+})
+
+*/
